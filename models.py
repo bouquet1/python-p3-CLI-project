692789387f4c7fe73ae8970613b982eb2d0e4c24
bouquet1 <bukuleta@gmail.com>
2023-10-01 23:29:45 -0700

    adds Store Classs model

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship, backref
 from sqlalchemy import Column, Integer, String, ForeignKey
 
 
@@ -10,20 +10,30 @@ class Company(Base):
     id = Column(Integer(), primary_key=True)
     company_name = Column(String())
 
+    stores = relationship('Store', backref='company')
+
     def __repr__(self):
-        return f"\n <Company "\
+        return f"<Company "\
             + f"id={self.id}," \
             + f"company_name={self.company_name}>"
+            
     
 class Store(Base):
     __tablename__ = "stores"
     
     id = Column(Integer(), primary_key=True)
-    company_id = Column(Integer(), )
+    address_line_1 = Column(String())
+    address_line_2 = Column(String())
+    apt_or_suite = Column(String())
+    city = Column(String())
+    state = Column(String())
+    zip_code = Column(String())
+    company_id = Column(Integer(), ForeignKey('companies.id'))
 
     def __repr__(self):
-        return f"\n <Company "\
+        return f"<Store "\
             + f"id={self.id}," \
-            + f"company_name={self.company_name}>"
+            + f"company_address={self.address_line_1}, {self.address_line_2},{self.apt_or_suite}, {self.city}, {self.state}, {self.zip_code}>"
+    
     
 
