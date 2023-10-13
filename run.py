d5de9cebb2449d52b888c6e5d6ca247b7e21ae74
bouquet1 <bukuleta@gmail.com>
2023-10-13 12:22:27 -0700

    adds run.py

---
+++ run.py
@@ -0,0 +1,17 @@
+
+
+def run():
+    print("Welcome to the mattress sales tracker!\n")
+    print("1.List of stores")
+    print("2.List of salespersons")
+    print("3.List of sales")
+    user_input = input("Please make a selection (1-3)")
+
+    handle_input(user_input)
+
+
+def handle_input(input):
+    import ipdb; ipdb.set_trace()
+
+
+run()
